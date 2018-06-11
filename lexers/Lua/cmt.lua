0ca68ef0e6c8fb14c608d31b05c8724028f8b5ba
Alexey-t <support@uvviewsoft.com>
2018-06-11 19:19:13 +0300

    Lua tests

---
+++ lexers/Lua/cmt.lua
@@ -0,0 +1,15 @@
+-- comment --[[test22]] comment continue
+
+--[[ comment]] not comment
+
+--[=[
+comment
+comment
+]=] not comment
+
+not comment
+
+--[[ comment
+comment
+comment end ]] not comment
+
