cd8c87645b28d7e7b62bb4e22fe6433604f55c40
Alexey-t <support@uvviewsoft.com>
2018-06-25 09:11:35 +0300

    up

---
+++ lexers/PHP/css in php.php
@@ -0,0 +1,24 @@
+<style type="text/css">
+table {
+    margin: 8px;
+}
+
+th {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: .7em;
+    background: #666;
+    color: #FFF;
+    padding: 2px 6px;
+    border-collapse: separate;
+    border: 1px solid #000;
+}
+
+td {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: .7em;
+    border: 1px solid #DDD;
+}
+</style>
+
+
+ddd
