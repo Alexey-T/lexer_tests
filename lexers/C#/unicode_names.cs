56094b00297d34307afb07dca52f0262019d3001
Alexey-t <support@uvviewsoft.com>
2020-02-05 17:17:47 +0300

    up

---
+++ lexers/C#/unicode_names.cs
@@ -0,0 +1,9 @@
+class ΔProgram
+{
+    static void ΔMain()
+    {
+        var Δ = 1;
+        Δ++;
+        ΔSystem.ΔConsole.WriteLine(Δ+20.33e-2);        
+    }
+}
---
+++ lexers/Inno_Setup/comments_pragma.iss
@@ -0,0 +1,13 @@
+;ddd
+  ;dfsdf
+    ;sdfsdf
+	;dddd
+	
+#pragma msg "ddd" \
+"ddd"  \
+"dddd"
+dddd
+
+#define aaa \
+"dddd"
+ddd
