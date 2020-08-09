bb6ad93289473064f539ea984de089d45ab8e077
Alexey <support@uvviewsoft.com>
2020-08-09 18:07:15 +0300

    +

--- lexers/Bash/[[ inside $().sh
+++ lexers/Bash/[[ inside $().sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+echo $( $(true) ) # not bag…
+pwd=$(echo $(pwd)) #bag… can't put space here
+
 echo $( [[ -n $1 ]] && echo Enable args || echo Not args)
-
 echo $( (($#)) && echo Enable args || echo Not args)
-
 echo $[9-$[4*$[1>0]]]
