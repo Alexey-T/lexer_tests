a8a00661456a83a54cab32c18562d8a34f1b3751
Alexey <support@uvviewsoft.com>
2021-01-07 15:42:16 +0300

    +

---
+++ lexers/C++/operators_all.cpp
@@ -0,0 +1,83 @@
+// https://en.wikipedia.org/wiki/Operators_in_C_and_C%2B%2B
+
+a = b
+a + b
+a - b
++a
+-a
+a * b
+a / b
+a % b
+++a
+a++
+--a
+a--
+a == b
+a != b
+a not_eq b[b]
+a > b
+a < b
+a >= b
+a <= b
+a <=> b
+!a
+not a[b]
+a && b
+a and b[b]
+a || b
+a or b[b]
+~a
+compl a[b]
+a & b
+a bitand b[b]
+a | b
+a bitor b[b]
+a ^ b
+a xor b[b]
+a << b
+a >> b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a &= b
+a and_eq b[b]
+a |= b
+a or_eq b[b]
+a ^= b
+a xor_eq b[b]
+a <<= b
+a >>= b 
+
+a[b]
+*a
+&a
+a->b
+a.b
+a->*b
+a.*b
+
+a(a1, a2)
+a, b
+a ? b : c
+a::b
+"a"_b
+sizeof(a)[i]
+sizeof(type)
+sizeof...(Args)
+alignof(type)
+or _Alignof(type)[j]
+typeid(a)
+typeid(type)
+(type)a
+type(a)
+static_cast<type>(a)
+dynamic_cast<type>(a)
+const_cast<type>(a)
+reinterpret_cast<type>(a)
+new type
+new type[n]
+delete a
+delete[] a
+noexcept(a)
