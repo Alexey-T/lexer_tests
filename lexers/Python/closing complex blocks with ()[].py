e1126e15e5f3e50a99fecef829acb07519398c5e
Alexey <support@uvviewsoft.com>
2021-01-08 20:59:19 +0300

    +

---
+++ lexers/Python/closing complex blocks with ()[].py
@@ -0,0 +1,28 @@
+    while True:
+        pass;               LOG and log('sels={}',sels)
+
+        cnts    = ([
+    # cmt
+    dict(              tp='lb'    ,tid='file'         ,l=GAP             ,w=70            ,cap=_('Export &to')                    )
+   ,dict(cid='file'   ,tp='ed'    ,t=GAP              ,l=GAP+70          ,r=GAP+WD_LST-35 ,en='0'                                 )
+   ,dict(cid='brow'   ,tp='bt'    ,tid='file'         ,l=GAP+HT_LST-35   ,r=GAP+WD_LST    ,cap=_('&...')                          )
+    ])
+    # cmt
+        pass;
+        pass;
+ 
+    pass;
+    pass;
+    pass;
+  
+    def tst():
+        v = [
+    #cmt
+    dd,
+    ee]
+    #cmt
+        pass;
+        pass;
+            
+    pass;
+    pass;
