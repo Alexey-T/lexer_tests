f81128f5053827405442298814cf5260b1026bae
Alexey <support@uvviewsoft.com>
2021-08-12 13:27:53 +0300

    +

---
+++ lexers/C++/asm/asm-blocks-MSVC.cpp
@@ -0,0 +1,14 @@
+asm{
+    mov al, 2;
+    out al, dx;
+}
+_asm{
+    mov al, 2;
+    out al, dx;
+}
+__asm{
+    mov al, 2;
+    out al, dx;
+}
+__asm mov al, 2 ;
+__asm mov dx, 0xD007 asm 0xff; __asm out dx, al;
