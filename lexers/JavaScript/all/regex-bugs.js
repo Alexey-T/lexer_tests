d5a6dcd88b4986ec95151177a81084677cec2cea
Alexey <support@uvviewsoft.com>
2022-01-30 14:55:04 +0300

    +

---
+++ lexers/JavaScript/all/regex-bugs.js
@@ -0,0 +1,35 @@
+0&&
+~await/a//*a
+//*/
+
+instanceof/a/*a
+//*/
+
+in/a/*a
+//*/
+
+if(0) for(a
+of/a/*a)a
+//*/
+
+~{*a(){yield
+/a/}}
+
+~class
+extends
+/\//
+{}
+/a
+
+0&&
+~class extends class{}{}/a//*
+//*/
+
+if(0) goto/a//*
+//*/
+if(0) GOTO/a//*
+//*/
+
+````/a//*
+//*/
+
