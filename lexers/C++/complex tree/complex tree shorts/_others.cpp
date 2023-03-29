f5ca41cf47d7a066a5aecac8f17ecc22d437ca7b
Alexey <support@uvviewsoft.com>
2023-03-29 15:39:26 +0300

    +

--- lexers/C++/complex tree/complex tree shorts/_others.cpp
+++ lexers/C++/complex tree/complex tree shorts/_others.cpp
@@ -1,24 +1,19 @@
-template <typename T1, typename T2> auto compose(T1 a, T2 b) -> decltype(a + b)
+template <typename T1, typename T2> auto f1(T1 a, T2 b) -> decltype(a + b)
 {
-   return a+b;
 }
 
 template<auto n> // C++17 auto parameter declaration
-auto f() -> std::pair<decltype(n), decltype(n)> // auto can't deduce from brace-init-list
+auto f2() -> std::pair<decltype(n), decltype(n)> // auto can't deduce from brace-init-list
 {
-    return {n, n};
 }
 
-auto make_var() -> int
+auto f3() -> int
 {
-  return 1;
 }
 
 //------------------------
-_NODISCARD mapped_type& at(const key_type& _Keyval) {
-    return _Loc._Bound->_Myval.second;
+_NODISCARD mapped_type& g1(const key_type& _Keyval) {
 }
 
-_NODISCARD const mapped_type& at(const key_type& _Keyval) const {
-    return _Loc._Bound->_Myval.second;
+_NODISCARD const mapped_type& g2(const key_type& _Keyval) const {
 }
