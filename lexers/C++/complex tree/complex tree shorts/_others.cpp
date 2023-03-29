7c7f96ca01d553fa079eb6c7836b7cdb35e22beb
Alexey <support@uvviewsoft.com>
2023-03-29 16:22:10 +0300

    +

--- lexers/C++/complex tree/complex tree shorts/_others.cpp
+++ lexers/C++/complex tree/complex tree shorts/_others.cpp
@@ -1,19 +1,39 @@
-template <typename T1, typename T2> auto f1(T1 a, T2 b) -> decltype(a + b)
-{
-}
+auto f0() {}
+auto f1() const {}
+auto f2() noexcept {}
+auto f3() : a0(0), a1(new b) #ifdef {}
+auto f4() & {}
+auto f5() && {}
+auto f10() -> int {}
+auto f11() -> decltype(a + b) {}
+auto f12() -> std::pair<decltype(n), decltype(n)> /*...*/ {}
 
-template<auto n> // C++17 auto parameter declaration
-auto f2() -> std::pair<decltype(n), decltype(n)> // auto can't deduce from brace-init-list
-{
-}
+auto Aa<T,TT>::f0() {}
+auto Aa<T,TT>::f1() const {}
+auto Aa<T,TT>::f2() noexcept {}
+auto Aa<T,TT>::f3() : a0(0), a1(new b) #ifdef {}
+auto Aa<T,TT>::f4() & {}
+auto Aa<T,TT>::f5() && {}
+auto Aa<T,TT>::f10() -> int {}
+auto Aa<T,TT>::f11() -> decltype(a + b) {}
+auto Aa<T,TT>::f12() -> std::pair<decltype(n), decltype(n)> /*...*/ {}
 
-auto f3() -> int
-{
-}
+auto Aa<T,TT>::Bb<T>::f0() {}
+auto Aa<T,TT>::Bb<T>::f1() const {}
+auto Aa<T,TT>::Bb<T>::f2() noexcept {}
+auto Aa<T,TT>::Bb<T>::f3() : a0(0), a1(new b) #ifdef {}
+auto Aa<T,TT>::Bb<T>::f4() & {}
+auto Aa<T,TT>::Bb<T>::f5() && {}
+auto Aa<T,TT>::Bb<T>::f10() -> int {}
+auto Aa<T,TT>::Bb<T>::f11() -> decltype(a + b) {}
+auto Aa<T,TT>::Bb<T>::f12() -> std::pair<decltype(n), decltype(n)> /*...*/ {}
 
-//------------------------
-_NODISCARD mapped_type& g1(const key_type& _Keyval) {
-}
-
-_NODISCARD const mapped_type& g2(const key_type& _Keyval) const {
-}
+auto Aa<T,TT>::Bb<T>::Cc::f0() {}
+auto Aa<T,TT>::Bb<T>::Cc::f1() const {}
+auto Aa<T,TT>::Bb<T>::Cc::f2() noexcept {}
+auto Aa<T,TT>::Bb<T>::Cc::f3() : a0(0), a1(new b) #ifdef {}
+auto Aa<T,TT>::Bb<T>::Cc::f4() & {}
+auto Aa<T,TT>::Bb<T>::Cc::f5() && {}
+auto Aa<T,TT>::Bb<T>::Cc::f10() -> int {}
+auto Aa<T,TT>::Bb<T>::Cc::f11() -> decltype(a + b) {}
+auto Aa<T,TT>::Bb<T>::Cc::f12() -> std::pair<decltype(n), decltype(n)> /*...*/ {}
