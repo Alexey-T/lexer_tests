3ee645120e3635ced1b807fd714f40a3c3e79060
Alexey <support@uvviewsoft.com>
2023-11-02 14:33:41 +0300

    +

---
+++ lexers/Zig/hello.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // If this program is run without stdout attached, exit with an error.
+    const2222 stdout_file = std.io.getStdOut();
+    // If this program encounters pipe failure when printing to stdout, exit
+    // with an error.
+    try stdout_file.writeAll("Hello, world!\n");
+}
