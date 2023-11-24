366460c50be762a5343acc8e8ce4a5a31deb0566
Alexey <support@uvviewsoft.com>
2023-11-24 10:24:28 +0300

    +

---
+++ lexers/Python/f-strings-py3.12.py
@@ -0,0 +1,17 @@
+   print(f"This is the playlist: {"\n".join(songs)}")
+
+   print(f"This is the playlist: {"\N{BLACK HEART SUIT}".join(songs)}")
+
+   print(f"This is the playlist: {", ".join([
+   ...     'Take me back to Eden',  # My, my, those eyes like fire
+   ...     'Alkaline',              # Not acid nor alkaline
+   ...     'Ascensionism'           # Take to the broken skies at last
+   ... ])}")
+   
+   print(f"""This is the playlist: {"\N{BLACK HEART SUIT}".join(songs)}""")
+
+   print(f"""This is the playlist: {", ".join([
+   ...     'Take me back to Eden',  # My, my, those eyes like fire
+   ...     'Alkaline',              # Not acid nor alkaline
+   ...     'Ascensionism'           # Take to the broken skies at last
+   ... ])}""")
