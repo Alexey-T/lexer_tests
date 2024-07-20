ee2d0ff4f29779584bde5e5d1b016f0f9677f7de
Alexey T <support@uvviewsoft.com>
2024-07-20 11:19:51 +0300

    +

---
+++ lexers/JavaScript/all/doc comments.js
@@ -0,0 +1,20 @@
+www
+/**Test*/
+www/*******www
+
+www*/*//**/www*/*/*/*/*/www
+www*/
+/**/d
+/***/d
+
+/**
+ * @typedef {{
+ *   measureTask: goog.dom.animationFrame.Task_,
+ *   mutateTask: goog.dom.animationFrame.Task_,
+ *   state: (!Object|undefined),
+ *   args: (!Array|undefined),
+ *   isScheduled: boolean
+ * }}
+ * @private
+ */
+dd
