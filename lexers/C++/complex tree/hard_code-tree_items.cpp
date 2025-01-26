f31cf43842ce27149b9907ab5638d9cf4915ce51
Alexey T. <support@uvviewsoft.com>
2025-01-26 07:56:44 +0300

    +

---
+++ lexers/C++/complex tree/hard_code-tree_items.cpp
@@ -0,0 +1,20 @@
+﻿//hard to catch code-tree nodes
+//1 level of nesting
+;
+CScriptEngine::CScriptEngine(CNppExec* pNppExec, const CListT<tstr>& CmdList, const tstr& id){}
+;
+CScriptEngine::~CScriptEngine(){}
+bool CScriptEngine::IsParentOf(const std::shared_ptr<CScriptEngine> pScriptEngine) const{}
+bool CScriptEngine::IsChildOf(const std::shared_ptr<CScriptEngine> pScriptEngine) const{}
+bool CScriptEngine::ContinueExecution() const{}
+std::shared_ptr<CChildProcess> CScriptEngine::GetRunningChildProcess(){}
+
+//2 levels of nesting
+;
+Aaa::CScriptEngine::CScriptEngine(CNppExec* pNppExec, const CListT<tstr>& CmdList, const tstr& id){}
+;
+Aaa::CScriptEngine::~CScriptEngine(){}
+bool Aaa::CScriptEngine::IsParentOf(const std::shared_ptr<CScriptEngine> pScriptEngine) const{}
+bool Aaa::CScriptEngine::IsChildOf(const std::shared_ptr<CScriptEngine> pScriptEngine) const{}
+bool Aaa::CScriptEngine::ContinueExecution() const{}
+std::shared_ptr<CChildProcess> Aaa::CScriptEngine::GetRunningChildProcess(){}
