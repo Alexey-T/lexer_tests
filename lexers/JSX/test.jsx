472de99e20a45e00ee4ed52e9eb74c1eaf22f748
Alexey T. <support@uvviewsoft.com>
2025-03-31 19:50:43 +0300

    +

---
+++ lexers/JSX/test.jsx
@@ -0,0 +1,16 @@
+﻿{(function () {
+  const guess = Math.random()
+
+  if (guess > 0.66) {
+    return <span style={{color: 'tomato'}}>Look at us.</span>
+  }
+
+  if (guess > 0.33) {
+    return <span style={{color: 'violet'}}>Who would have guessed?!</span>
+  }
+
+  return <span style={{color: 'goldenrod'}} alt="test">Not me.</span>
+})()}
+
+<test/>test</test>
+
