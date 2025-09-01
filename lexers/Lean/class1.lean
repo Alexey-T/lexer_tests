7ef742a492e2a90117f4c786dbcb264cee2098c9
Alexey T. <support@uvviewsoft.com>
2025-09-01 14:12:54 +0300

    +

---
+++ lexers/Lean/class1.lean
@@ -0,0 +1,19 @@
+﻿-- Define a typeclass 'Showable' with one method 'show'
+class Showable (α : Type) where
+  show : α → String
+
+-- Implement 'Showable' for the 'Nat' type
+instance : Showable Nat where
+  show n := s!"This is a Nat: {n}"
+
+-- Implement 'Showable' for the 'String' type
+instance : Showable String where
+  show s := s!"This is a String: {s}"
+
+-- A function that uses the 'Showable' typeclass
+def display {α : Type} (x : α) [Showable α] : String :=
+  show x
+
+-- Example usage
+#eval display 42      -- "This is a Nat: 42"
+#eval display "hello" -- "This is a String: hello"
