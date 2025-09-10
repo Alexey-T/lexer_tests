c6d0eff83d17d391569552424df3c0de8bc19a1c
Alexey T. <support@uvviewsoft.com>
2025-09-10 19:53:30 +0300

    +

---
+++ lexers/C++/complex tree/complex tree 1/hard_code-tree_2.cpp
@@ -0,0 +1,20 @@
+﻿CNppExecMacroVars::StrCalc::StrCalc(tstr& varValue, CNppExec* pNppExec)
+  : m_varValue(varValue), m_pNppExec(pNppExec), m_calcType(CT_FPARSER), m_pVar(0)
+{}
+
+bool CNppExecMacroVars::substituteMacroVar(const tstr& Cmd, tstr& S, int& pos,
+                                           const TCHAR* varName,
+                                           tstr (*getValue)(CNppExec* pNppExec) )
+{}
+
+CNppExecMacroVars::CNppExecMacroVars() : m_pNppExec(0)
+{}
+
+template<> bool OperandComparator<tstr>::eq_i() const
+{}
+
+template<class MacroVarFunc> bool IterateUserMacroVars(
+    typename MacroVarFunc::container_type& userMacroVars,
+    typename MacroVarFunc::container_type& userLocalMacroVars,
+    MacroVarFunc func)
+{}
